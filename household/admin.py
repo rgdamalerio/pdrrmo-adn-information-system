238158ff937718cb0dc6f4a6c7eaaa7f50abc17e
Roel Damalerio <damalerioroel@gmail.com>
2022-08-11 05:32:53 +0800

    Added  readonly fields, arrange field in household form, added list of filer

--- household/admin.py
+++ household/admin.py
@@ -5,10 +5,20 @@ from leaflet.admin import LeafletGeoAdmin
 # Register your models here.
 # Register your models here.
 class HouseholdsAdmin(LeafletGeoAdmin):
+  readonly_fields = ('enumerator','editor',)
+  fields = ['respondent','municipality', 'barangay', 'purok', 'location','householdbuildingtypes',
+            'householdtenuralstatus','year_construct','estimated_cost', 'number_bedrooms', 'number_storey',
+            'access_electricity', 'householdroofmaterials','householdwallmaterials','medical_treatment',
+            'access_water_supply','potable','householdwatertenuralstatus','waterlevelsystems','floods_occur',
+            'year_flooded','experience_evacuate','year_evacuate','evacuationareas','access_health_medical_facility',
+            'access_telecommuniciation','access_drill_simulation','image','enumerator','editor'
+           ]
   list_display = ("controlnumber","municipality","barangay","purok","respondent",
     "date_interview","created_at","updated_at","owner")
   search_fields = ("respondent",)
-  
+  list_filter = ('municipality_id','barangay_id','access_electricity', 'access_internet','access_water_supply','potable',
+    'floods_occur','experience_evacuate','access_health_medical_facility',
+    'access_telecommuniciation','access_drill_simulation')
 
 admin.site.register(Households,HouseholdsAdmin)
 
