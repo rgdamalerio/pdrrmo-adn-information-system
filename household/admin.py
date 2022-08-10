367922d50771bc704c1259b3b1b21330b536f0ab
Roel Damalerio <damalerioroel@gmail.com>
2022-08-10 23:22:00 +0800

    update household model to upload initial data, update household admin settings. apply search field

--- household/admin.py
+++ household/admin.py
@@ -1,4 +1,3 @@
-import imp
 from django.contrib import admin
 from .models import Households
 from leaflet.admin import LeafletGeoAdmin
@@ -8,5 +7,27 @@ from leaflet.admin import LeafletGeoAdmin
 class HouseholdsAdmin(LeafletGeoAdmin):
   list_display = ("controlnumber","municipality","barangay","purok","respondent",
     "date_interview","created_at","updated_at","owner")
+  search_fields = ("respondent",)
+  
 
 admin.site.register(Households,HouseholdsAdmin)
+
+# def has_change_permission(self, request, obj=None):
+#         if obj:
+#             if obj.municipality_id == "160212000":
+#                  return False
+#             else:
+#                  return True
+#                  #return request.user.has_perm('Update Panaytayon')
+#         return super(HouseholdsAdmin, self).has_change_permission(self, request, obj)
+# def has_view_permission(self, request, obj=None):
+#   return False
+
+# def has_add_permission(self, request):
+#   return False
+
+# def has_change_permission(self, request, obj=None):
+#   return False
+
+# def has_delete_permission(self, request, obj=None):
+#   return False
