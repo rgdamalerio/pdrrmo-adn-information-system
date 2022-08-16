65326c0b4b17e9aec4d4d2f7366eb876b040080a
Roel Damalerio <damalerioroel@gmail.com>
2022-08-16 09:24:50 +0800

    Update default route as admin page

--- household_geodatabase/urls.py
+++ household_geodatabase/urls.py
@@ -21,5 +21,5 @@ admin.site.site_title = "Household Database Admin Portal"
 admin.site.index_title = "Welcome to Household Database"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("/", admin.site.urls),
 ]
