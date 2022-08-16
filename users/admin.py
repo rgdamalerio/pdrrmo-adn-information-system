64bb53cf34eccfc2693365b76ba3a332d372812b
Roel Damalerio <damalerioroel@gmail.com>
2022-08-16 11:21:57 +0800

    Update user admin configuration, setup UserProfile as inline stack in the form, unregister User default view and register back User view with UserProfile field

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from users.models import UserProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 
 # Register your models here.
+class UserProfileInline(admin.StackedInline):
+  model = UserProfile
+  can_delete: False
+
+class AccountsUserAdmin(AuthUserAdmin):
+  inlines = [UserProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User,AccountsUserAdmin)
