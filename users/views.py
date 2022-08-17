738e9a781eb9ef7f0e32b08b6b11d48b81594f96
Roel Damalerio <damalerioroel@gmail.com>
2022-08-17 16:41:44 +0800

    create a view for user model

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+
+from .serializer import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+  permission_classes = (IsAuthenticated,)
+  serializer_class = UserSerializer
+  queryset = get_user_model().objects.all()
