15deb660abe4a603290bd9e304cb842778c87ab8
Roel Damalerio <damalerioroel@gmail.com>
2022-08-17 16:41:03 +0800

    Update settings.py added rest_framework.authtoken in installed apps, add REST_FRAMEWORK settings

--- household_geodatabase/settings.py
+++ household_geodatabase/settings.py
@@ -45,11 +45,24 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.gis",
     "rest_framework",
+    "rest_framework.authtoken",
     "users",
     "leaflet",
     "library",
     "household",
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
