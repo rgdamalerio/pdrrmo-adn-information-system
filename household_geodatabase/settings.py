13a9e30f44d8e71dc29c8514cdbbddad37e7fb77
Roel Damalerio <damalerioroel@gmail.com>
2022-09-27 10:08:54 +0800

    Remove Tile Group control in template

--- household_geodatabase/settings.py
+++ household_geodatabase/settings.py
@@ -168,6 +168,8 @@ LEAFLET_CONFIG = {
     'ATTRIBUTION_PREFIX': 'Operation and Warning Division',
     'TILES': [('Esri_WorldImagery', 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
           'attribution': 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+        }),('OpenStreetMap', 'http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          'attribution': '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
         }),
         ('Drak Map', 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
             'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
