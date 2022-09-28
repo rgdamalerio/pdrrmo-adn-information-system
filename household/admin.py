11c519347202eb285de9f8fa86c28791d70d61d5
Roel Damalerio <damalerioroel@gmail.com>
2022-09-28 14:25:11 +0800

    Overrides leaflet widget configuration

--- household/admin.py
+++ household/admin.py
@@ -32,6 +32,18 @@ class LivelihoodsInline(admin.StackedInline):
 # Register your models here.
 @admin.register(Households)
 class HouseholdsAdmin(LeafletGeoAdmin):
+  settings_overrides = {
+    'TILES': [('Esri_WorldImagery', 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+          'attribution': 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+        }),('OpenStreetMap', 'http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          'attribution': '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        }),
+        ('Drak Map', 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+            'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            'subdomains': 'abcd',
+            'maxZoom': 19
+        })],
+  }
   readonly_fields = ('enumerator','editor',)
   fields = ['respondent','municipality', 'barangay', 'purok', 'location','householdbuildingtypes',
             'householdtenuralstatus','year_construct','estimated_cost', 'number_bedrooms', 'number_storey',
