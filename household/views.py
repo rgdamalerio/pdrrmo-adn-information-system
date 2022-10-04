38aeb7d28daf6930e5a881ab788d29796752354e
Roel Damalerio <damalerioroel@gmail.com>
2022-10-05 04:39:04 +0800

    Refactor logic in filtering base on query string set by the user, more clearer and redable code

--- household/views.py
+++ household/views.py
@@ -6,21 +6,31 @@ from django.core.exceptions import PermissionDenied
 from django.views.decorators.http import require_http_methods
 from .forms import HouseholdSearchForm
 
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
 @require_http_methods(["GET"])
 def household_datasets(request):
   
   if request.user.is_authenticated:
-    # Do something for authenticated users.
-    print(request.GET)
-    filter_params = {}
-    if request.GET.get('controlnumber'):
-      filter_params['controlnumber'] = request.GET.get('controlnumber')
-    elif request.GET.get('purok'):
-      filter_params['purok'] = request.GET.get('purok')
-    else:
-      pass
+    # # Do something for authenticated users.
+    # print(request.GET)
+    # filter_params = {}
+    # if request.GET.get('controlnumber'):
+    #   filter_params['controlnumber'] = request.GET.get('controlnumber')
+    # elif request.GET.get('purok'):
+    #   filter_params['purok'] = request.GET.get('purok')
+    # else:
+    #   pass
 
-    households = serialize('geojson',Households.objects.filter(**filter_params))
+    # households = serialize('geojson',Households.objects.filter(**filter_params))
+    qs = Households.objects.all()
+    controlnumber_contains_query = request.GET.get('controlnumber')
+
+    if is_valid_queryparam(controlnumber_contains_query):
+      qs = qs.filter(controlnumber__icontains=controlnumber_contains_query)
+      
+    households = serialize('geojson',qs)
     return HttpResponse(households,content_type='json')
    
   else:
