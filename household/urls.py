7f0a2f9da3dac7b557599ee45fd3eca406f57536
Roel Damalerio <damalerioroel@gmail.com>
2022-11-07 16:45:26 +0800

    Created view,url and template for graphs

--- household/urls.py
+++ household/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import household_datasets, household_search_form, household_info
+from .views import household_datasets, household_search_form, household_info, chart_view
 
 urlpatterns = [
     path('household_data/', household_datasets, name='household'),  
     path('household_info/', household_info, name='householdinfo'),  
     path('household_search_form/', household_search_form, name='household_search_form'),  
+    path('household_chart/', chart_view, name='household_chart'),  
 ]
