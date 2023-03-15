b99a0cc397064be8db06fa2b9eb31a3e06a72a69
HarlequinSamoranos2001 <hsamoranosaclcbxu@gmail.com>
2023-03-15 09:31:06 +0800

    Update on chained-dropdown purok

--- household/forms.py
+++ household/forms.py
@@ -1,14 +1,14 @@
 from email.policy import default
 from django import forms
 from household.models import Households
-from library.models import Municipalities, Barangays
+from library.models import Municipalities, Barangays, Purok
 from library.views import municipality_list
 
 class HouseholdForm(forms.ModelForm):
     class Meta:
         model = Households
 
-        fields = ['municipality', 'barangay']
+        fields = ['municipality', 'barangay', 'purok_id']
 
     def __init__(self, *args, **kwargs):
         super(HouseholdForm, self).__init__(*args, **kwargs)
@@ -23,13 +23,21 @@ class HouseholdForm(forms.ModelForm):
 
         # Barangay is on the child level, it will be loaded when user click the parent level
         try:
-          self.initial['barangays'] = kwargs['instance'].barangays.psgccode
-          barangay_init_form = [(i.psgccode, i.brgyname) for i in Barangays.objects.filter(
-                municipality=kwargs['instance'].municipality
-            )]
+            self.initial['barangays'] = kwargs['instance'].barangays.psgccode
+            barangay_init_form = [(b.psgccode, b.brgyname) for b in Barangays.objects.filter(
+                    municipality=kwargs['instance'].municipality
+                )]
         except:
             barangay_init_form = [('', '---------')]
-
+        
+        try:
+            self.initial['purok'] = kwargs['instance'].purok.purok_id
+            purok_init_form = [(p.purok_id, p.purok_name) for p in Purok.objects.filter(
+                    barangay=kwargs['instance'].barangay
+                )]
+        except:
+            purok_init_form = [('', '---------')]
+ 
         
         # Override the form, add onchange attribute to call the ajax function
         self.fields['municipality'].widget = forms.Select(
@@ -49,6 +57,14 @@ class HouseholdForm(forms.ModelForm):
             },
             choices=barangay_init_form,
         )
+
+        self.fields['purok_id'].widget = forms.Select(
+           attrs={
+           'style': 'width:200px'
+           },
+            choices=purok_init_form,
+        )
+
 
 class HouseholdSearchForm(forms.ModelForm):
     class Meta:
