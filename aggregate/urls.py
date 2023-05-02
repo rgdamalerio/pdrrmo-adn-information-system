1599efe17b6ee7b2f7fbe1c117509bdd3d0fb4ca
HarlequinSamoranos2001 <hsamoranosaclcbxu@gmail.com>
2023-05-02 11:35:06 +0800

    comment

--- aggregate/urls.py
+++ aggregate/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = "aggregate"
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     path('export_aggregate_xls', views.exportFamilyandPopulation, name='export_aggregate'),
     #path('print_aggregate', views.print_aggregate, name="print_aggregate"),
     # path('<int:id>/generatePDF/', views.generatePDF, name='generatePDF'),
