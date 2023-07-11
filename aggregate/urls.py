cba5fb60fec6b93494c44840060c722d36022311
Harlequin A. Samoranos <123434874+HarlequinSamoranos2001@users.noreply.github.com>
2023-07-11 15:30:46 +0800

    Update urls.py

--- aggregate/urls.py
+++ aggregate/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
 from . import views 
+from reports.views import export_infant_counts
 
 
 app_name = "aggregate"
 urlpatterns = [
     path('chart-data', views.chart_view, name='chart_data'),
-    path('export_aggregate_xls', views.exportFamilyandPopulation, name='export_aggregate'),
+    path('export_aggregate_xls', views.export_infant_counts, name='export_aggregate'),
     path('population_data', views.get_data, name='population_data'),
     #path('print_aggregate', views.print_aggregate, name="print_aggregate"),
     # path('<int:id>/generatePDF/', views.generatePDF, name='generatePDF'),
