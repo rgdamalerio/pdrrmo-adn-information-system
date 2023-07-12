5e320ecb337f194c8d9320c2fa4bd630d02d9e67
HarlequinSamoranos2001 <hsamoranosaclcbxu@gmail.com>
2023-07-12 08:48:53 +0800

    Revert "Revert "Merge branch 'master' into dev""
    This reverts commit 7eb87952f90fd37b8e359b794886addd6d750844.

--- aggregate/urls.py
+++ aggregate/urls.py
@@ -8,12 +8,8 @@ urlpatterns = [
     path('chart-data', views.chart_view, name='chart_data'),
     path('export_aggregate_xls', views.exportFamilyandPopulation, name='export_aggregate'),
     path('population_data', views.get_data, name='population_data'),
+]
     #path('print_aggregate', views.print_aggregate, name="print_aggregate"),
     # path('<int:id>/generatePDF/', views.generatePDF, name='generatePDF'),
     #path('export_pdf', views.export_pdf, name='export_pdf'),
      #path('aggregated_data', views.aggregated_data, name='aggregated_data'),
-]
-
-# urlpatterns = [
-#     path("", views.some, name='some'),
-# ]
